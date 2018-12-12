763d72095d94674b7fc2dcc1bc5cd5b7ed4398c2
dirandiran <diran90@mail.ru>
2018-12-12 19:28:39 +0500

    Add files via upload

---
+++ src/main/java/ru/homework/testurlservice/TestUrlserviceApplication.java
@@ -0,0 +1,14 @@
+package ru.homework.testurlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUrlserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestUrlserviceApplication.class, args);
+	}
+
+}
+
